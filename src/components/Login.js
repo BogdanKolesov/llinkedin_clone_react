f4b849b4ff966bde396ebcec7a2ba3ac41682de1
Bogdan <bogdan_kolesov@bk.ru>
2021-11-09 20:56:08 +0600

    fix: Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -124,6 +124,7 @@ const Hero = styled.div`
         height: 670px;
         position: absolute;
         bottom: -2px;
+        
         right: -150px;
         @media (max-width: 768px){
             top: 230px;
@@ -135,11 +136,34 @@ const Hero = styled.div`
 `
 
 const Form = styled.div`
-
+    margin-top: 100px;
+    width: 480px;
+    @media (max-width: 768px){
+        margin-top: 20px;
+    }
 `
 
 const Google = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ffffff;
+    height: 56px;
+    width: 100%;
+    border-radius: 28px;
+    box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%), 
+                inset 0 0 0 2px rgb(0 0 0 /0%), 
+                inset 0 0 0 1px rgb(0 0 0 / 0%);
+    vertical-align: middle;
+    z-index: 0;
+    transition-duration: 167ms;
+    font-size: 20px;
+    color: rgba(0,0,0, 0.6);
 
+    &:hover{
+        background-color: rgba(207,207,207, 0.25);
+        color: rgba(0,0,0,0.75)
+    }
 `
 
 export default Login
