26bd7e889f3259195a68ac46f529758e7e43c5a7
Bogdan <bogdan_kolesov@bk.ru>
2021-11-10 21:42:23 +0600

    fix: Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { navData } from './data/navData'
 
 const Header = () => {
     return (
@@ -18,6 +19,40 @@ const Header = () => {
                         <img src="/assets/images/search-icon.svg" alt="search" />
                     </SearchIcon>
                 </Search>
+                <Nav>
+                    <NavListWrap>
+                        {
+                            navData.map((item, index) => (
+                                <NavList key={index}>
+                                    <a>
+                                        <img src={item.source} alt={item.alt} />
+                                        <span>{item.text}</span>
+                                    </a>
+                                </NavList>
+                            ))
+                        }
+                        <User>
+                            <a >
+                                <img src="/assets/images/user.svg" alt="user" />
+                                <span>Me</span>
+                                <img src="/assets/images/down-icon.svg" alt="down" />
+                            </a>
+                            <SignOut>
+                                <a>Sign Out</a>
+                            </SignOut>
+                        </User>
+
+                        <Work>
+                            <a>
+                                <img src="/assets/images/nav-work.svg" alt="Work" />
+                                <span>
+                                    Work
+                                    <img src="/assets/images/down-icon.svg" alt="down" />
+                                </span>
+                            </a>
+                        </Work>
+                    </NavListWrap>
+                </Nav>
             </Content>
         </Container >
     );
@@ -84,6 +119,113 @@ const SearchIcon = styled.div`
     justify-content: center;
     align-items: center;
 `
+const Nav = styled.nav`
+    margin-left: auto;
+    display: block;
+    @media (max-width: 768px){
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+    }
+`
+
+const NavListWrap = styled.ul`
+    display: flex;
+    flex-wrap: nowrap;
+    list-style-type: none;
+
+`
+
+const NavList = styled.li`
+    display: flex;
+    align-items: center;
+    a{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: transparent;
+        flex-direction: column;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 1.5;
+        min-height: 42px;
+        min-width: 80px;
+        position: relative;
+        text-decoration: none;
+        span{
+        color: rgba(0,0,0,0.6);
+        display: flex;
+        align-items: center;
+        
+    }
+        @media(max-width:768px){
+            min-width: 70px;
+        }
+    }
+    &:hover, 
+    &:active{
+        a{
+            span{
+                color: rgba(0,0,0,0.9);
+            }
+        }
+    }
+    .active{
+        span:after{
+            content: '';
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white, #ffffff);
+            bottom: 0;
+            left: 0;
+            position: absolute;
+            border-color: rgba(0,0,0,0.9);
+        }
+    }
+`
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: #ffffff;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+    cursor: pointer;
+`
+
+const User = styled(NavList)`
+    a > svg{
+        width: 24px;
+        border-radius: 50%;
+    }
+    a > img{
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+    span{
+        display: flex;
+        align-items: center;
+    }
+
+    &:hover{
+        ${SignOut}{
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+`
+
+const Work = styled(User)`
+    border-left: 1px solid rgba(0,0,0,0.08);
+`
+
+
 
 
 export default Header;
---
+++ src/components/data/navData.js
@@ -0,0 +1,28 @@
+export const navData = [
+    {
+        source: '/assets/images/nav-home.svg',
+        alt: 'Home',
+        text: 'Home'
+    },
+    {
+        source: '/assets/images/nav-network.svg',
+        alt: 'Network',
+        text: 'My Network'
+    },
+    {
+        source: '/assets/images/nav-jobs.svg',
+        alt: 'Jobs',
+        text: 'Jobs'
+    },
+    {
+        source: '/assets/images/nav-messaging.svg',
+        alt: 'Messagge',
+        text: 'Message'
+    },
+    {
+        source: '/assets/images/nav-notifications.svg',
+        alt: 'Notifications',
+        text: 'Notifications'
+    }
+
+]
