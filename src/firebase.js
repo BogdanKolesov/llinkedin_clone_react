e4f424aa91fbf6765eff330617b40e399980d386
Bogdan <bogdan_kolesov@bk.ru>
2021-11-19 20:03:40 +0600

    add: firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZCcASrIP5uBMS_cptgfQqb3WXzT3M42U",
+  authDomain: "linkedinclonereact.firebaseapp.com",
+  projectId: "linkedinclonereact",
+  storageBucket: "linkedinclonereact.appspot.com",
+  messagingSenderId: "440084646236",
+  appId: "1:440084646236:web:a26371db91a63ca7759999"
+
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+const auth 
