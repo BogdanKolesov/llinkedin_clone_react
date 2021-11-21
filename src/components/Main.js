00adaf59bb1c3ab5437f7afa6bba8be483c69a87
Bogdan <bogdan_kolesov@bk.ru>
2021-11-21 16:04:12 +0600

    fix: PostModal

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PostModal from './PostModal'
 
-const Main = () => {
+const Main = (props) => {
     return (
         <Container>
             <ShareBox>
--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -4,7 +4,14 @@ import styled from 'styled-components';
 const PostModal = () => {
     return (
         <Container>
-            PostModal
+            <Content>
+                <Header>
+                    <h2>Create a post</h2>
+                    <button>
+                        <img src='/assets/images/close-icon.svg' alt='close' />
+                    </button>
+                </Header>
+            </Content>
         </Container>
     );
 };
@@ -17,7 +24,50 @@ const Container = styled.div`
     bottom: 0;
     z-index: 9999;
     color: #000000;
-    background-color: #000000;
+    background-color: rgba(0,0,0,0.6);
+`
+
+const Content = styled.div`
+    width: 100%;
+    max-width: 555px;
+    background-color: #ffffff;
+    max-height: 90%;
+    overflow: initial;
+    border-radius: 5px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    top: 32px;
+    margin: 0 auto;
+`
+
+const Header = styled.div`
+    display: block;
+    padding: 16px 20px;
+    border-bottom: 1px solid rgba(0,0,0,0.15);
+    font-size: 16px;
+    line-height: 1.5;
+    color: rgba(0,0,0,0.6);
+    font-weight: 400;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    button{
+        height: 40px;
+        width: 40px;
+        min-width: auto;
+        color: rgba(0,0,0,0.15);
+        background: transparent;
+        border: none;
+        cursor: pointer;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &:hover{
+            background-color: rgba(0,0,0,0.15);
+        }
+    }
 `
 
 export default PostModal;
